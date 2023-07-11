c0a4d1e495313f549c887b2c7773c6e7842114f2
Ujjwal Kumar <ujjwal13818@gmail.com>
2023-07-12 02:03:19 +0530

    added front-end with react

---
+++ beginner/src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from './../../Assets/logo.jpeg'
+import './Nav.css';
+const Nav = () => {
+    return(
+        <div className="nav-container">
+            <div className="nav-left">
+              <img className="flash-logo" src={logo} alt="logo" />
+              <p className="flash-logo-text">
+                Typofast
+              </p>
+            </div>
+              <div className="nav-right">
+                <a 
+                target="_blank"
+                rel="noreferrer"
+                className="nav-linkedin-link"
+                href="http://localhost:2500/features">Go Back</a>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
